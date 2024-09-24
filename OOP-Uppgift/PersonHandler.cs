7df136b8443f71d72ec1f55aafd2fa2ff801d018
Dania Tamim <dania.tamim@live.se>
2024-09-24 10:39:35 +0200

    Incapsulate Person class with PersonHandler.

--- OOP-Uppgift/PersonHandler.cs
+++ OOP-Uppgift/PersonHandler.cs
@@ -8,5 +8,24 @@ namespace OOP_Uppgift
 {
     internal class PersonHandler
     {
+        public Person CreatePerson(string fname, string lname, int age, int height, int weight)
+        {
+            Person person = new Person(fname, lname, age, height, weight);
+            return person;
+
+
+        }
+
+
+        public void SetAge(Person person, int age)
+        {
+            person.Age = age;
+        }
+
+        internal void printPerson(Person p1)
+        {
+            Console.WriteLine(p1.ToString());
+
+        }
     }
 }
--- OOP-Uppgift/Program.cs
+++ OOP-Uppgift/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace OOP_Uppgift
+﻿using System.Security.Cryptography;
+
+namespace OOP_Uppgift
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // FirstPart();
+            SecondPart();
 
+        }
+
+        private static void SecondPart()
+        {
+            PersonHandler personHandler = new PersonHandler();
+            Person p1 = personHandler.CreatePerson("Nisse", "Nilsson", 20, 178, 60);
+            personHandler.printPerson(p1);
+            personHandler.SetAge(p1, 25);
+            personHandler.printPerson(p1);
+        }
+
+        private static void FirstPart()
+        {
             try
             {
                 Person person = new Person("Kalle", "Testson", 37, 170, 70);
@@ -17,8 +34,6 @@
 
                 Console.WriteLine(ex.Message);
             }
-
-
         }
     }
 }
